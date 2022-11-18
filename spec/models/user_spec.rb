4a134192eb7c3622711175ce91a8fe1370409778
bspatafora <benjamin.spatafora@gmail.com>
2022-11-18 16:42:13 -0600

    Add User specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "creates users with valid params" do
+    user_params = {
+      name: "a" * 70,
+      phone: "+15555555555",
+    }
+
+    expect { User.create!(user_params) }.not_to raise_error
+  end
+
+  it "limits names to 70 characters" do
+    user_params = {
+      name: "a" * 71,
+      phone: "+15555555555",
+    }
+
+    expect { User.create!(user_params) }.to raise_error(ActiveRecord::ValueTooLong)
+  end
+
+  it "limits phones to 12 characters" do
+    user_params = {
+      name: "a" * 70,
+      phone: "+155555555559",
+    }
+
+    expect { User.create!(user_params) }.to raise_error(ActiveRecord::ValueTooLong)
+  end
 end

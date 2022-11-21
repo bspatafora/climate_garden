2b96ee81d895c59cc19ece3862645e481812a771
bspatafora <benjamin.spatafora@gmail.com>
2022-11-21 10:37:05 -0600

    Configure rubocop

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,28 +1,30 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "creates users with valid params" do
+  it 'creates users with valid params' do
     user_params = {
-      name: "a" * 70,
-      phone: "+15555555555",
+      name: 'a' * 70,
+      phone: '+15555555555'
     }
 
     expect { User.create!(user_params) }.not_to raise_error
   end
 
-  it "limits names to 70 characters" do
+  it 'limits names to 70 characters' do
     user_params = {
-      name: "a" * 71,
-      phone: "+15555555555",
+      name: 'a' * 71,
+      phone: '+15555555555'
     }
 
     expect { User.create!(user_params) }.to raise_error(ActiveRecord::ValueTooLong)
   end
 
-  it "limits phones to 12 characters" do
+  it 'limits phones to 12 characters' do
     user_params = {
-      name: "a" * 70,
-      phone: "+155555555559",
+      name: 'a' * 70,
+      phone: '+155555555559'
     }
 
     expect { User.create!(user_params) }.to raise_error(ActiveRecord::ValueTooLong)

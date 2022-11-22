756088a461791e356d66797737a7861b78c9ca62
bspatafora <benjamin.spatafora@gmail.com>
2022-11-22 17:41:46 -0600

    Add Users request specs

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,6 +3,20 @@
 require 'rails_helper'
 
 RSpec.describe 'Users' do
+  describe 'GET /show' do
+    let(:user) { create(:user) }
+
+    it 'returns http success' do
+      get "/users/#{user.id}"
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'returns data associated with the user' do
+      get "/users/#{user.id}"
+      expect(response.body).to include(user.name)
+    end
+  end
+
   describe 'GET /new' do
     it 'returns http success' do
       get '/users/new'
@@ -11,17 +25,16 @@ RSpec.describe 'Users' do
   end
 
   describe 'POST /create' do
+    let(:user_params) { { name: 'Usie McUserson', phone: '+15555555555' } }
+
     it 'returns http success' do
-      post '/users', params: { user: { name: 'Usie McUserson', phone: '+15555555555' } }
+      post '/users', params: { user: user_params }
       follow_redirect!
       expect(response).to have_http_status(:success)
     end
-  end
 
-  describe 'GET /show' do
-    it 'returns http success' do
-      get "/users/#{create(:user).id}"
-      expect(response).to have_http_status(:success)
+    it 'saves the user to the database' do
+      expect { post '/users', params: { user: user_params } }.to change(User, :count).by(1)
     end
   end
 end

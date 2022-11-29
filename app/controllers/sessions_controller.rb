9621e5e83cb890716f715703c3d80c03379bb320
bspatafora <benjamin.spatafora@gmail.com>
2022-11-29 16:21:19 -0600

    Add Sessions controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class SessionsController < ApplicationController
+  def new; end
+
+  # def create
+  # end
+
+  # def destroy
+  # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   resources :users, only: %i[new create show]
+  resources :sessions, only: %i[new create destroy]
 end

6f783957c353887bfe429b1f902bcb4d42a1d810
bspatafora <benjamin.spatafora@gmail.com>
2022-12-06 16:22:12 -0600

    Test sessions

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -3,10 +3,33 @@
 require 'rails_helper'
 
 RSpec.describe 'Sessions' do
+  let(:user) { create(:user) }
+
   describe 'GET /new' do
     it 'returns http success' do
-      get '/sessions/new'
+      get '/sessions/new', params: { phone: user.phone }
       expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'POST /create' do
+    let(:otp) { user.one_time_passwords.create }
+
+    it 'returns http success' do
+      post '/sessions', params: { phone: user.phone, otp: otp.value }
+      follow_redirect!
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'stores the user’s ID in the session' do
+      post '/sessions', params: { phone: user.phone, otp: otp.value }
+      follow_redirect!
+      expect(session[:user_id]).to eq(user.id)
+    end
+
+    it 'redirects to the user’s page' do
+      post '/sessions', params: { phone: user.phone, otp: otp.value }
+      expect(response).to redirect_to(user_path(user))
     end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -25,14 +25,22 @@ RSpec.describe 'Users' do
   end
 
   describe 'POST /create' do
+    let(:user_attributes) { attributes_for(:user) }
+
     it 'returns http success' do
-      post '/users', params: { user: attributes_for(:user) }
+      post '/users', params: { user: user_attributes }
       follow_redirect!
       expect(response).to have_http_status(:success)
     end
 
     it 'saves the user to the database' do
-      expect { post '/users', params: { user: attributes_for(:user) } }.to change(User, :count).by(1)
+      expect { post '/users', params: { user: user_attributes } }.to change(User, :count).by(1)
+    end
+
+    it 'redirects to the user’s page' do
+      post '/users', params: { user: user_attributes }
+      user = User.find_by(phone: user_attributes[:phone])
+      expect(response).to redirect_to(user_path(user))
     end
   end
 end

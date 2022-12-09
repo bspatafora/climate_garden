5b9b41a54ce1b1dea2f6ac769d5111fdd213f74a
bspatafora <benjamin.spatafora@gmail.com>
2022-12-09 16:34:14 -0600

    Test OneTimePassword.valid?

--- app/models/one_time_password.rb
+++ app/models/one_time_password.rb
@@ -13,15 +13,16 @@ class OneTimePassword < ApplicationRecord
   validates :value, presence: true
   validates :expires_at, presence: true
 
-  scope :valid, -> { where('expires_at > ?', Time.current) }
+  scope :unexpired, -> { where('expires_at > ?', Time.current) }
 
   def self.random_six_digit
     (SecureRandom.random_number(9e5) + 1e5).to_i
   end
 
-  # TODO: Test
   def self.valid?(value, user)
-    latest_valid = where(user:).valid.order(created_at: :desc).first
+    latest_valid = where(user:).unexpired.order(created_at: :desc).first
+
+    return false if latest_valid.nil?
 
     value == latest_valid.value
   end
--- spec/models/one_time_password_spec.rb
+++ spec/models/one_time_password_spec.rb
@@ -6,13 +6,11 @@ RSpec.describe OneTimePassword do
   describe 'value' do
     it 'disallows nil' do
       otp = build(:one_time_password, value: nil)
-
       expect { otp.save! }.to raise_error(ActiveRecord::RecordInvalid)
     end
 
     it 'defaults to a random, six-digit number' do
       otp = create(:one_time_password)
-
       expect(otp.value.length).to eq(6)
     end
   end
@@ -20,13 +18,11 @@ RSpec.describe OneTimePassword do
   describe 'expires_at' do
     it 'disallows nil' do
       otp = build(:one_time_password, expires_at: nil)
-
       expect { otp.save! }.to raise_error(ActiveRecord::RecordInvalid)
     end
 
     it 'defaults to some future time' do
       otp = create(:one_time_password)
-
       expect(otp.expires_at).to be > Time.current
     end
   end
@@ -34,30 +30,57 @@ RSpec.describe OneTimePassword do
   describe 'user' do
     it 'disallows invalid foreign key' do
       otp = build(:one_time_password, user_id: 0)
-
       expect { otp.save! }.to raise_error(ActiveRecord::RecordInvalid)
     end
 
     it 'destroys on deletion' do
       user = create(:user)
       otp = create(:one_time_password, user:)
+
       user.destroy!
 
       expect { otp.reload }.to raise_error(ActiveRecord::RecordNotFound)
     end
   end
 
-  describe '#valid' do
-    it 'returns true if otp has not yet expired' do
+  describe 'unexpired' do
+    it 'returns true if OTP has not yet expired' do
       otp = create(:one_time_password, expires_at: 1.second.from_now)
-
-      expect(described_class.where(user: otp.user).valid).to exist
+      expect(described_class.where(user: otp.user).unexpired).to exist
     end
 
-    it 'returns false if otp has expired' do
+    it 'returns false if OTP has expired' do
       otp = create(:one_time_password, expires_at: 1.second.ago)
+      expect(described_class.where(user: otp.user).unexpired).not_to exist
+    end
+  end
 
-      expect(described_class.where(user: otp.user).valid).not_to exist
+  describe 'valid?' do
+    it 'returns true if value matches the user’s newest, unexpired OTP' do
+      otp = create(:one_time_password)
+      expect(described_class.valid?(otp.value, otp.user)).to be true
+    end
+
+    it 'returns false if value matches OTP of another user' do
+      otp = create(:one_time_password)
+      other_user = create(:user)
+      expect(described_class.valid?(otp.value, other_user)).to be false
+    end
+
+    it 'returns false if value does not match the user’s newest, unexpired OTP' do
+      otp = create(:one_time_password)
+      expect(described_class.valid?('wrong', otp.user)).to be false
+    end
+
+    it 'returns false if value matches the user’s newest, but expired, OTP' do
+      otp = create(:one_time_password, expires_at: 1.second.ago)
+      expect(described_class.valid?(otp.value, otp.user)).to be false
+    end
+
+    it 'returns false if value matches an unexpired, but not newest, OTP of the user’s' do
+      otp1 = create(:one_time_password)
+      otp1.user.one_time_passwords.create
+      expect(described_class.valid?(otp1.value, otp1.user)).to be false
     end
   end
 end
